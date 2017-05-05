33a8e7742cb011af3f361ddcbd58c6857bbfe63b
Diane Zevenbergen <dianezev@comcast.net>
2017-05-05 14:28:14 -0700

    disable node.js feature until API key fully implemented

--- public/js/indexV1.js
+++ public/js/indexV1.js
@@ -7,52 +7,69 @@
  * Note LISTINGS from NPR do not require an API Key
  * but STORIES do require a key.
  *
- * To keep the API key private when requesting STORIES,
- * use node.js/Express to make a server-side request.
- * (TBD: verify that it won't show up in url, etc.)
+ * If data requiring an API key is requested via CLIENT, then
+ * API key is stored in this file & visible to user in browser.
+ * (TBD later: what if key is kept in PHP file?)
  *
- * For LISTINGS (which do not require API key), make request
- * from client.
+ * If request is from SERVER via node.js/Express, I think
+ * API key is not visible to user since it can be stored in
+ * the server.js file (and there's no need for it to be defined in this client file).
  *
- * *
+ * Question: When data is requested via node.js/Express, will API key display
+ * in the url?
+ *
  * Examples below include the following types of requests from CLIENT:
  *      1. GET & POST ajax calls for XML data (for NPR listing with no key)
  *      2. GET & POST ajax calls for JSON data (for NPR listing with no key)
+ *      3. GET ajax call for JSON data (for NPR story with API key)
  *
  * Examples below include the following types of requests from SERVER, 
  *      using node.js/Express routing: (all of these request NPR stories 
- *      which require API key, which is embedded in server file app.js)
+ *      which require API key, which is embedded in server file)
  *      1. .ajax call (method GET) for JSON data (includes data property in request object)
  *      2. .get call for JSON data (passes query object in .get call)
  *      3. .ajax call (method POST) for JSON data (query string is in url)
  *
  */
 
-'use strict';
 
 $(function(){
     var template = $('#template').html();
     var href = 'http://api.npr.org/';
-    var listParser;
-        
-    var $goQuery = $('[id^=goQuery]');
+    
+    /*
+     * The apiKey here is used to make http request from CLIENT for articles,
+     * which makes the key visible in the browser.
+     * Note some OTHER examples below request articles
+     * FROM the server using node.js/Express routing which I think 
+     * can keep API key private 
+     *(since apiKey can be defined in app.js server file instead)
+     */
+    var apiKey = 'MDI0NjI0NTU3MDE0NjYwMjE0ODFjMGU1OQ000';
+    
+    var $goQuery = $('#goQuery');
     var $articleOpt = $('#articleOptions');
     var $listOpt = $('#listOptions');
+    var $radios = $('input:radio[name=npr_type]');
     var $result = $('#result ul');
-    var $resultDesc = $('h1.describe');
     var $error = $('#result p.error');
-    var categ;
 
-    // Initially hide details
-    $('[id$=Options]').hide();
-    $resultDesc.hide();
+    if($radios.is(':checked') === false) {
+        $radios.filter('[value=lists]').prop('checked', true);
+        $articleOpt.hide();
+    }
     
-    // Hide/show options based on user selection
-    $('[id^=tog]').click(function() {
-        var idToShow = this.nextElementSibling.id;
-
-        $('[id$=Options]').hide();
-        $('#' + idToShow).show();
+    // Bind click event for radio buttons
+    $radios.change(function(e) {
+        if (e.target.id === 'getArticles') {
+            $radios.filter('[value=articles]').prop('checked', true);
+            $listOpt.hide(); 
+            $articleOpt.show();
+        } else {
+            $radios.filter('[value=lists]').prop('checked', true);
+            $articleOpt.hide();
+            $listOpt.show();
+        }
         clearResults();
     });
 
@@ -76,26 +93,9 @@ $(function(){
         var reqObject;
         var myRoute;
 
-        // Remove this temp code when node.js implemented live
-        // This TEMPORARY if statement prevents the Node.js code in this function from running.
-        // I'm not making the Node.js portion live just yet because API key needs to be private
-        // Also see documentation at https://github.com/dianezev/RESTful_Demo/blob/master/README.md
-        if ($articleOpt.is(':visible') === true) {
-          alert("An API key from NPR is needed to use this feature. Please see https://github.com/dianezev/RESTful_Demo/blob/master/README.md for documentation.");
-          return;
-        }
-        
-      
-      
-        $('body').addClass('wait');
-        
-        $resultDesc.text('Please wait...');
-        $resultDesc.show();
-
         // Request data for listing depending on user options
         if ($listOpt.is(':visible') === true) {
             id = $('select[name=lists]').val();
-            categ=$('select[name=lists] option:selected').text();
             output = $('select[name=format]').val();
             method = $('select[name=method]').val();
             
@@ -125,26 +125,46 @@ $(function(){
 
             $.ajax(reqObject);
             
-        // Note that API key is req'd to get stories
-        // See temporary code at top of this function which disables this for version posted to web
-        // Also see documentation at https://github.com/dianezev/RESTful_Demo/blob/master/README.md
+        // Note that API key is req'd to get articles
         } else if ($articleOpt.is(':visible') === true) {
-          
             id = $('select[name=articles]').val();
-            categ=$('select[name=articles] option:selected').text();
             keyMethod = $('select[name=keyMethod]').val();
             output = 'JSON';
             
             
             /*
-             * Code samples: These methods get the same data in slightly 
-             * different ways, but all use node.js with API Key in
-             * the server side node file.
+             * Code samples: These methods get the same data in different ways.
              *
+             * First method processes the request from client side & 
+             * other methods process from server side (To confirm: I believe the 
+             * API key can be kept private when request happens on server side
+             * using node.js/Express routing
+             * since the key can be defined in the server file & could be excluded
+             * from this file which is visible in browser).
+             */
+            if (keyMethod === 'GET1') {                
+                reqObject = {
+                    url: href + 'query?' +
+                        'id=' + id +
+                        '&output=' + output +
+                        '&apiKey=' + apiKey,
+                    dataType: output,
+                    method: 'GET',
+                    success: function (data, state, res) {
+                        showResults(data, res, storyFromJSON);
+                    },
+                    error: function (data, state) {
+                        showError(state);
+                    }
+                };
+                
+                $.ajax(reqObject);
+            
+            /*
              * Use node.js/Express routing to process GET request on server side.
              * This example uses AJAXs' data property to pass query object.
              */
-            if (keyMethod === 'GET2') {
+            } else if (keyMethod === 'GET2') {
 
                 reqObject = {url: '/cors',
                             method: 'GET',
@@ -178,9 +198,6 @@ $(function(){
             }
         }
 
-        // Hide options
-        $('[id$=Options]').hide();        
-
         e.preventDefault();
     });
 
@@ -188,7 +205,6 @@ $(function(){
     function clearResults() {
         $result.text('');
         $error.text('');
-        $resultDesc.hide();
     }
 
     /*
@@ -197,22 +213,17 @@ $(function(){
     function showResults (data, res, parser) {
         var results = '';
 
-        $resultDesc.text('NPR listing of "' + categ + '"');
-        
         if (data) {
             results = parser(data);
             $result.html(results);
         } else {
             $result.text('STATUS CODE: ' + res.status);
-        }
-        $('body').removeClass('wait');
+        }                
     }    
     
     function showError (state) {
-        $resultDesc.text('Error occurred...');
         $result.addClass('error');
         $error.text(state);
-        $('body').removeClass('wait');
     }
 
     /*
@@ -225,7 +236,7 @@ $(function(){
         var listing = '';
 
         for (var i = 0, l = allTitles.length; i < l; i++){
-            listing += '<li><p class="title">' + allTitles[i].getElementsByTagName('title')[0].textContent + '</p>'
+            listing += '<li><strong>' + allTitles[i].getElementsByTagName('title')[0].textContent + '</strong><br>'
             + allTitles[i].getElementsByTagName('additionalInfo')[0].textContent + '</li><br>';
         }
 
@@ -242,11 +253,37 @@ $(function(){
         var listing = '';
 
         for (var i = 0, l = dataArray.length; i < l; i++){
-            listing += '<li><p class="title">' + dataArray[i].title.$text + '</p>'
+            listing += '<li><strong>' + dataArray[i].title.$text + '</strong><br>'
             + dataArray[i].additionalInfo.$text + '</li><br>';
         }
 
         return listing;
+    }
+
+    /*
+     * Called by fcn showResults
+     * Parses JSON data returned from AJAX call from CLIENT
+     * Based on NPR API format information for stories
+     */
+    function storyFromJSON(data) {
+        var dataArray = data.list.story;
+        var story = '';
+        var paraArray;
+
+        for (var i = 0, l = dataArray.length; i < l; i++) {
+            paraArray = dataArray[i].text.paragraph;
+            story += '<li><strong>' + dataArray[i].title.$text + '</strong><br>';
+            for (var j = 0, m = paraArray.length; j < m; j++) { 
+                if (typeof paraArray[j].$text !== 'undefined') {
+                    story += '&nbsp;&nbsp;&nbsp;&nbsp;' 
+                            + paraArray[j].$text
+                            + '<br><br>';
+                }
+            }
+            story += '</li><br>';
+        }
+        
+        return story;
     }
 
     /******************************************************************************************
@@ -257,14 +294,11 @@ $(function(){
      * handle AJAX calls from SERVER using node.js.
      */
     function nodeSuccess (results) {
-        $resultDesc.text('NPR stories for "' + categ + '"');
-
         if (results) {
             $result.html(results);
         } else {
             $result.text('STATUS CODE: ' + res.status);
         }
-        $('body').removeClass('wait');
     }
 });
     
